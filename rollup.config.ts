8b0225580eaeb620ad36516c893f192c25d74504
Eve-Sama <panshiming@exiao.tech>
2022-06-22 15:05:11 +0800

    build: 避免把第三方应用打包进来

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,12 +4,13 @@ const typescript = require('@rollup/plugin-typescript');
 const commonjs = require('@rollup/plugin-commonjs');
 
 const config: RollupOptions = {
-  input: 'src/index.ts',
+  input: './src/index.ts',
   output: {
     file: 'dist/bundle.js',
     format: 'cjs',
   },
   plugins: [commonjs(), typescript()],
+  external: id => !/^[./]/.test(id),
 };
 
 export default config;
